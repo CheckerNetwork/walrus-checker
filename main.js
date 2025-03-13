82a9df0b4ead11204e5dbca8f012e6592d2ecc71
Srdjan S <stankovic.srdjo@gmail.com>
2025-03-13 11:29:12 +0100

    Catch errors in main loop

--- main.js
+++ main.js
@@ -7,15 +7,20 @@ import { pickRandomItem } from './lib/random.js'
 import { MEASUREMENT_DELAY } from './lib/constants.js'
 
 while (true) {
-  const blobs = await getRecentBlobs()
-  console.log(`Found ${blobs.length} blobs`)
-  const measurement = await measure(
-    pickRandomItem(DEFAULT_AGGREGATORS),
-    pickRandomItem(blobs)
-  )
-  console.log('measurement:', measurement)
-  // TODO: Submit measurement to API
-  Zinnia.jobCompleted()
+  try {
+    const blobs = await getRecentBlobs()
+    console.log(`Found ${blobs.length} blobs`)
+    const measurement = await measure(
+      pickRandomItem(DEFAULT_AGGREGATORS),
+      pickRandomItem(blobs)
+    )
+    console.log('measurement:', measurement)
+    // TODO: Submit measurement to API
+    Zinnia.jobCompleted()
+  } catch (err) {
+    console.error('Error:', err)
+  }
+
   console.log(`Waiting ${MEASUREMENT_DELAY / 1_000} seconds...`)
   await new Promise(resolve => setTimeout(resolve, MEASUREMENT_DELAY))
 }
